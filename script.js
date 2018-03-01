0284a812e2b9765e4cd43c26f3e7cc8d99ed9ebe
Konrad Kościelski <konrad.koscielski@interia.pl>
2018-03-01 12:39:48 +0100

    init

---
+++ script.js
@@ -0,0 +1,23 @@
+var Counter = React.createClass({
+    getInitialState: function() {
+        return {
+            counter: 0
+        };
+    },
+
+    decrement: function() {
+        this.setState({
+            counter: this.state.counter - 1
+        });
+    },
+
+    render: function() {
+        return React.createElement('div', {onClick: this.decrement},
+            React.createElement('span', {}, 'Licznik ' + this.state.counter)
+            React.createElement('span', {}, 'Licznik ' -this.state.counter)
+        );
+    }
+});
+
+var element = React.createElement(Counter);
+ReactDOM.render(element, document.getElementById('app'));
